00a893be73f1e11be3b9953b810b74c3fc3e0570
wajdi-amer <wajdi-amer@cyber4s.dev>
2023-02-07 00:44:28 +0200

    changed Date format to show time ago using Moment library

--- client/src/components/Posts/Post/PostHeader/PostHeader.js
+++ client/src/components/Posts/Post/PostHeader/PostHeader.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 import './styles.css';
 
 const PostHeader = ({ postData }) => {
+
+    const dateTimeAgo = moment(new Date(postData.date)).fromNow();
     return (
         <div className='post-header-container'>
             <img src={postData.avatar} className='avatar' alt='avatar'></img>
             <div className='info-container'>
                 <h3 className='username'>{postData.username}</h3>
                 <div>
-                    <a href={`/shop/${postData.shopId}`}>{postData.shopName}</a> · {postData.date}
+                    <a href={`/shop/${postData.shopId}`}>{postData.shopName}</a> · <span className='date'>{dateTimeAgo}</span>
                 </div>
             </div>
         </div>
