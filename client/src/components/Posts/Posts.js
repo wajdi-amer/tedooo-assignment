cd761756be146dbc0bf98bafa4c9d0267eb00a5e
wajdi-amer <wajdi-amer@cyber4s.dev>
2023-02-07 22:31:15 +0200

    Infinite scrolling implemented

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Post from './Post/Post';
 
@@ -9,6 +10,7 @@ const baseURL = 'https://dev.tedooo.com/feed.json';
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [postsScrollIndex, setPostsScrollIndex] = useState(0);
 
     useEffect(() => {
         axios.get(baseURL).then((response) => {
@@ -16,16 +18,28 @@ const Posts = () => {
         });
     }, []);
 
+
+    const handleLoadMore = () => {
+        if (posts?.length > postsScrollIndex) setPostsScrollIndex(postsScrollIndex + 6);
+    }
+
     // In case there are no posts, the page will render a message.
     if (posts.length === 0) return (<h2>Cannot Find Posts...</h2>);
 
-  return (
-    <div className='posts-container'>
-        {posts?.map((post) => (
-            <Post postData={post} key={post.id}/>
-        ))}
-    </div>
-  )
+    return (
+        <InfiniteScroll
+            pageStart={0}
+            loadMore={handleLoadMore}
+            hasMore={true || false}
+        >
+            <div className='posts-container'>
+                {posts?.map((post, i) => {
+                    if (i < postsScrollIndex) return (<Post postData={post} key={post.id} />)
+                }
+                )}
+            </div>
+        </InfiniteScroll>
+    )
 }
 
 export default Posts;
