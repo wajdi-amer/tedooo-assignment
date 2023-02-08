21b5f3724f07b6aeeb8929ef3cd68c1a5fd9ca0e
wajdi-amer <wajdi-amer@cyber4s.dev>
2023-02-08 02:04:46 +0200

    firing an impression when a user watches an item using react-intersection-observer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import Posts from "./components/Posts/Posts";
 import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Posts />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Posts />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useSearchParams } from 'react-router-dom';
 
 import PostHeader from './PostHeader/PostHeader';
 import PostContent from './PostContent/PostContent';
@@ -7,8 +9,16 @@ import PostFooter from './PostFooter/PostFooter';
 import './styles.css';
 
 const Post = ({ postData }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { ref, inView } = useInView({
+    threshold: 0.8,
+    onChange: (inView) => {
+      if (inView) setSearchParams({ userId: postData.userId, itemId: postData.id });
+    }
+  });
+
   return (
-    <div className='post-container'>
+    <div className='post-container' ref={ref}>
       <PostHeader postData={postData} key={postData.userId} />
       <PostContent postData={postData} />
       <PostFooter postData={postData} />
